679d7a9b28115c83e82f501d5b2df07aa8c0386a
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2021-07-08 10:39:10 +0500

    Fix UserFactory faker calling

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class UserFactory extends Factory
 {
@@ -20,11 +20,14 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
+        /** @var \Faker\Generator $unique */
+        $unique = $this->faker->unique();
+
         return [
-            'name'              => $this->faker->name,
-            'email'             => $this->faker->unique()->safeEmail,
+            'name'              => $this->faker->name(),
+            'email'             => $unique->safeEmail(),
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
